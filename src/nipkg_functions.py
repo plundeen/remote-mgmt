a2679bd361440bbcaadb1fc9da7c975cccf60859
Phil Lundeen <plundeen@tae.com>
2021-09-24 14:12:20 -0700

    Adds register_feed function; cleans up imports

--- src/nipkg_functions.py
+++ src/nipkg_functions.py
@@ -1,5 +1,4 @@
-from subprocess import Popen, PIPE, STDOUT, subprocess
-import datetime
+import subprocess
 from typing import List
 
 # Constants
@@ -13,17 +12,19 @@ def query(arg: str) -> str:
 
 def update_cache() -> None:
     """Update the local nipkg cache"""
-    subprocess.run([NIPKG, 'update'], shell=True)
+    subprocess.run([NIPKG, "update"], shell=True)
 
 
 def build_package_list() -> None:
     """Generates a file containing the list of available package names"""
+    # Dumping to file for now, in case we decide to just
+    # do this periodically in the background
     update_cache()
-    with Popen(
+    with subprocess.Popen(
         [NIPKG, "list"],
         shell=True,
-        stdout=PIPE,
-        stderr=STDOUT,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         bufsize=1,
         universal_newlines=True,
     ) as p:
@@ -35,15 +36,16 @@ def build_package_list() -> None:
             f.write("\n".join(packages))
 
 
-def get_package_versions(package:str) -> List[str]:
+def get_package_versions(package: str) -> List[str]:
     """Returns a list of available versions for the specified package"""
+    # Update the cache to check for newly-available versions
     update_cache()
     versions = []
-    with Popen(
+    with subprocess.Popen(
         [NIPKG, "list", package],
         shell=True,
-        stdout=PIPE,
-        stderr=STDOUT,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         bufsize=1,
         universal_newlines=True,
     ) as p:
@@ -55,6 +57,11 @@ def get_package_versions(package:str) -> List[str]:
     return versions
 
 
+def register_feed(name: str) -> None:
+    """Registers a new feed so that contained packages can be installed"""
+    subprocess.run([NIPKG, "update", f"--name={name}"], shell=True)
+
+
 def install(package: str, version: str) -> None:
     """Function to trigger a package install and stream the stdout/stderr output"""
     # Question: can we provide a pipe as an input that this function can stream to?
@@ -62,4 +69,5 @@ def install(package: str, version: str) -> None:
     # Or should this function just handle the full websocket stuff?
     pass
 
-#get_package_versions("canary")
+
+# get_package_versions("canary")
