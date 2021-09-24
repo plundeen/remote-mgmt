ce060deece6aaee2c149670b8971a6d4156fd552
Phil Lundeen <plundeen@tae.com>
2021-09-24 09:38:53 -0700

    Adds streaming text example, to demo live package installation stdout

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, stream_with_context
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
@@ -6,6 +6,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Email
 from time import sleep
+from datetime import datetime
 
 # dummy package names for now
 package_list = ["canary", "demo", "foo", "bar"]
@@ -14,6 +15,7 @@ version_list = ["1.0.0.0", "1.1.0.0", "1.2.3.4", "2.0.1.0"]
 
 class FeedRegistrationForm(FlaskForm):
     """Flask WTForm object for selecting feed to register"""
+
     feed = SelectField(
         label="Package", choices=package_list, validators=[DataRequired()]
     )
@@ -22,6 +24,7 @@ class FeedRegistrationForm(FlaskForm):
 
 class InstallForm(FlaskForm):
     """Flask WTForm object for selecting package to install"""
+
     package = SelectField(
         label="Package", choices=package_list, validators=[DataRequired()]
     )
@@ -44,7 +47,7 @@ def mynavbar():
         View("Register Feed", "register_feed"),
         View("Install Package", "install_package"),
         View("Job History", "job_history"),
-        View("Stream test", "stream_test")
+        View("Stream test", "stream_test"),
     )
 
 
@@ -87,14 +90,29 @@ def install_package():
 def job_history():
     return render_template("job_history.html")
 
+
+def stream_template(template_name, **context):
+    """Function to stream data to a template"""
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.disable_buffering()
+    return rv
+
+
+def generate():
+    """Demo function to simulate streaming data (such as package installation stdout)"""
+    for i in range(10):
+        yield f"{datetime.now().time()} Line {i}"
+        sleep(0.5)
+
+
 @app.route("/stream_test", methods=["GET"])
 def stream_test():
     # https://flask.palletsprojects.com/en/2.0.x/patterns/streaming/
-    def generate():
-        for i in range(10):
-            yield f"Line {i}<br>"
-            sleep(0.5)
-    return app.response_class(generate())
+    # https://gist.github.com/huiliu/46be335427605960fa84
+    lines = generate()
+    return Response(stream_with_context(stream_template("stream_test.html", lines=lines)))
 
 
 if __name__ == "__main__":
