3364970dd2af57e1600047630cac8c5dbf47385c
Phil Lundeen <plundeen@tae.com>
2021-09-25 17:31:34 -0700

    Maintain map of clients

--- src/websocket_server.py
+++ src/websocket_server.py
@@ -1,25 +1,30 @@
 import websockets
 import asyncio
+import json
 
 PORT = 7890
 
 print(f"Server listening on port {PORT}")
 
-clients = set()
+# Create a dictionary/hash table of connected clients
+clients = {}
 
 async def echo(websocket, path):
     print(f"New client connected: {websocket}")
-    clients.add(websocket)
     try:
         async for message in websocket:
             print(f"Received message from client: {message}")
+            msg_obj = json.loads(message)
+            if msg_obj.get("id"):
+                clients[websocket] = msg_obj["id"]
+                print(clients)
             for client in clients:
                 await client.send(f"Someone said: {message}")
     except websockets.exceptions.ConnectionClosed as e:
         print("Client disconnected")
         print(e)
     finally:
-        clients.remove(websocket)
+        del clients[websocket]
 
 start_server = websockets.serve(echo, "localhost", PORT)
 
